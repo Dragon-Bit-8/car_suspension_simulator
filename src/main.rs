8987bba592a794580515a24d2de7b1859085a330
Saúl Hernández <dragonpunishment123@gmail.com>
2025-09-23 17:57:00 -0600

    Lo mismo de antes pero con nuevo formato

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,11 @@ struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_systems(Startup, add_people)
-            .add_systems(Update, (update_people, greet_people).chain());            
+            .add_systems(Update, (update_people, greet_people).chain());
     }
 }
-
-
 
 fn main() {
     App::new()
@@ -29,22 +26,22 @@ fn main() {
         .run();
 }
 
-fn add_people(mut commands: Commands){
-    commands.spawn((Person,Name(String::from("Juanito"))));
-    commands.spawn((Person,Name(String::from("Pedrito"))));
-    commands.spawn((Person,Name(String::from("Sutanito"))));
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name(String::from("Juanito"))));
+    commands.spawn((Person, Name(String::from("Pedrito"))));
+    commands.spawn((Person, Name(String::from("Sutanito"))));
 }
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>,query: Query<&Name, With<Person>>){
-    if timer.0.tick(time.delta()).just_finished(){
-        for name in &query{
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
             println!("Hello {}!", name.0);
         }
     }
 }
 
-fn update_people(mut query: Query<&mut Name, With<Person>>){
-    for mut name in &mut query{
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
         if name.0 == "Juanito" {
             name.0 = String::from("Perenganito");
             break;
